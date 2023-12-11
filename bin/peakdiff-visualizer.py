190348d010cabb74d9b0d9462c997bab99f6199e
Cong Wang <cwang31@stanford.edu>
2023-12-11 11:54:49 -0800

    Bookkeep the running script.

--- bin/peakdiff-visualizer.py
+++ bin/peakdiff-visualizer.py
@@ -1,28 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from peakdiff.data   import DataSource
-from peakdiff.viewer import Viewer
+from peakdiff.data   import CXIPeakDiff
+from peakdiff.viewer import CXIPeakDiffViewer
 
 from dataclasses import dataclass
 
-data_source_config_args = {
-    "path_n_peaks_A" : "peaknet.n_peaks.msgpack",
-    "path_m_rates_A" : "peaknet.m_rate.msgpack",
-    "path_n_peaks_B" : "pyalgo.n_peaks.msgpack",
-    "path_m_rates_B" : "pyalgo.m_rate.msgpack",
-}
+path_cxi_0 = 'cxic00318.aggregated.cxi'
+path_cxi_1 = 'inference_results/peaknet.cxic00318_0123.cxi'
 
-@dataclass
-class DataSourceConfig:
-    path_n_peaks_A: str
-    path_m_rates_A: str
-    path_n_peaks_B: str
-    path_m_rates_B: str
+cxi_peakdiff = CXIPeakDiff(path_cxi_0, path_cxi_1)
 
-data_source_config = DataSourceConfig(**data_source_config_args)
-data_source = DataSource(data_source_config).init_config()
-
-viewer = Viewer(data_source = data_source)
+viewer = CXIPeakDiffViewer(cxi_peakdiff = cxi_peakdiff)
 
 viewer.run()
